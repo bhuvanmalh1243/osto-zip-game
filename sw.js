1c38c4ab42be411d12f863ab3627254cb9694e4d
bhuvanmalh1243 <bhuvan.m@osto.one>
2026-09-23 19:54:09 +0530

    Quick play: name is required to claim a goodie, no exit from that screen on a win

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='osto-zip-v12';
+const C='osto-zip-v13';
 const F=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>Promise.all(F.map(u=>
