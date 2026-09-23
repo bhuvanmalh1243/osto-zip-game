20a2fd35def28122238489c111eaaa1ee26e13e2
bhuvanmalh1243 <bhuvan.m@osto.one>
2026-09-23 12:37:07 +0530

    Work-email-only validation with admin override

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='osto-zip-v6';
+const C='osto-zip-v7';
 const F=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(C).then(c=>Promise.all(F.map(u=>
